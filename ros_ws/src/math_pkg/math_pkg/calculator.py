0967f305cbc2ff96238118f28fb949eb04e5d24d
robodev8g <developer8g@gmail.com>
2024-09-06 11:15:40 +0300

    Make calculator directly sending exercises with timer

--- ros_ws/src/math_pkg/math_pkg/calculator.py
+++ ros_ws/src/math_pkg/math_pkg/calculator.py
@@ -2,7 +2,12 @@ import rclpy
 from rclpy.node import Node
 
 from my_interfaces.srv import Exercise
+from std_srvs.srv import Trigger
 
+from rclpy.callback_groups import MutuallyExclusiveCallbackGroup, ReentrantCallbackGroup
+from rclpy.executors import MultiThreadedExecutor
+
+from threading import Event
 
 class Calculator(Node):
 
@@ -12,8 +17,12 @@ class Calculator(Node):
         self.declare_parameter('operator', '+')
         self.declare_parameter('b', 0)
 
+
         self.client = self.create_client(Exercise, 'solve')
-        self.send_exercise()
+
+        # self.create_service(Trigger, 'use_calculator_command', self.use_calculator_command_callback, callback_group=self.rcb)
+        self.timer = self.create_timer(1, self.send_exercise)
+
 
     def send_exercise(self):
         # Create a service request
@@ -23,11 +32,38 @@ class Calculator(Node):
         request.num2 = self.get_parameter('b').get_parameter_value().integer_value
 
         # Send the request and wait for the response
+        self.get_logger().info("send an exercise to server")
         future = self.client.call_async(request)
-        rclpy.spin_until_future_complete(self, future)
+        future.add_done_callback(self.print_result)
 
-        response = future.result()
-        self.get_logger().info(f"Response: {response}")
+    def print_result(self, future):
+        exercise_response: Exercise.Response  = future.result()
+        self.get_logger().info(f"Response: {exercise_response}")
+
+    # def use_calculator_command_callback(self, calculator_request: Trigger.Request, calculator_response: Trigger.Response):
+    #     self.get_logger().info("get trigger to send new exercise")
+
+    #     event=Event()
+    #     def done_callback(future):
+    #         self.get_logger().info(f"done callback")
+    #         nonlocal event
+    #         event.set()
+        
+    #     future = self.send_exercise()
+    #     self.get_logger().info(f"exit send_exercise method")
+    #     future.add_done_callback(done_callback)
+
+    #     event.wait()
+
+    #     # rclpy.spin_until_future_complete(self, executor=self.executor, future=future)
+    #     exercise_response: Exercise.Response  = future.result()
+    #     self.get_logger().info(f"Response: {exercise_response}")
+    #     calculator_response.success = exercise_response.success
+
+    #     # calculator_response.success = True
+    #     calculator_response.message = "exercise was calculated successfully" if calculator_response.success else "illegal exercise"
+    #     return calculator_response
+
 
 
 
