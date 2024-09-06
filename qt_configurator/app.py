f8e4fbf4e5d4813f17bfb39bdd09e5deb361525f
robodev8g <developer8g@gmail.com>
2024-09-06 11:57:11 +0300

    Establishe a zenoh session in app

--- qt_configurator/app.py
+++ qt_configurator/app.py
@@ -3,11 +3,15 @@ from PyQt5.QtWidgets import QApplication, QWidget
 from PyQt5 import uic
 from PyQt5.QtCore import Qt
 
+import zenoh
+
 class MyWidget(QWidget):
     def __init__(self):
         super().__init__()
-        self.ui = uic.loadUi("/home/user/projects/ros-parameters-qt-config/qt_configurator/configurator.ui")
-        
+        self.ui = uic.loadUi("/home/user/projects/ros-parameters-qt-config/qt_configurator/config/configurator.ui")
+
+        self.session = zenoh.open(zenoh.config.Config.from_file("/home/user/projects/ros-parameters-qt-config/qt_configurator/config/qt_configurator_session.json5"))
+
 
     def keyPressEvent(self, event):
         if event.key() == Qt.Key_Escape:
